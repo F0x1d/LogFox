4326af12ad87a4e8a0bbd3228e42addeb9434f10
Jesse205 <2055675594@qq.com>
2023-10-03 06:08:04 +0800

    fix: settings insets

--- app/src/main/java/com/f0x1d/logfox/ui/fragment/settings/base/BasePreferenceFragment.kt
+++ app/src/main/java/com/f0x1d/logfox/ui/fragment/settings/base/BasePreferenceFragment.kt
@@ -6,17 +6,28 @@ import androidx.navigation.fragment.findNavController
 import androidx.preference.PreferenceFragmentCompat
 import com.f0x1d.logfox.R
 import com.f0x1d.logfox.extensions.isHorizontalOrientation
+import com.f0x1d.logfox.extensions.paddingRelative
+import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.appbar.MaterialToolbar
 import dev.chrisbanes.insetter.applyInsetter
 
-abstract class BasePreferenceFragment: PreferenceFragmentCompat() {
+abstract class BasePreferenceFragment : PreferenceFragmentCompat() {
 
     open val title = R.string.settings
     open val showBackArrow = false
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        val context = requireContext()
+        view.findViewById<AppBarLayout>(R.id.app_bar_layout).apply {
+            applyInsetter {
+                type(statusBars = true, navigationBars = true, displayCutout = true) {
+                    paddingRelative(
+                        context, start = !context.isHorizontalOrientation, top = true, end = true
+                    )
+                }
+            }
+        }
         view.findViewById<MaterialToolbar>(R.id.toolbar).apply {
             setTitle(this@BasePreferenceFragment.title)
 
@@ -30,8 +41,13 @@ abstract class BasePreferenceFragment: PreferenceFragmentCompat() {
             clipToPadding = false
 
             applyInsetter {
-                type(navigationBars = true) {
-                    padding(vertical = requireContext().isHorizontalOrientation)
+                type(navigationBars = true, displayCutout = true) {
+                    paddingRelative(
+                        context,
+                        start = !context.isHorizontalOrientation,
+                        bottom = context.isHorizontalOrientation,
+                        end = true
+                    )
                 }
             }
         }
