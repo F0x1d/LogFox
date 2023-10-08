5be5b02a270a8255282382b7daf68be9a467f813
Jesse205 <2055675594@qq.com>
2023-10-09 06:54:30 +0800

    fix: use = instead {}

--- app/src/main/java/com/f0x1d/logfox/extensions/FABExtensions.kt
+++ app/src/main/java/com/f0x1d/logfox/extensions/FABExtensions.kt
@@ -4,9 +4,8 @@ import androidx.annotation.StringRes
 import androidx.appcompat.widget.TooltipCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-fun FloatingActionButton.setDescription(@StringRes resId: Int) {
-    setDescription(context.getString(resId))
-}
+fun FloatingActionButton.setDescription(@StringRes resId: Int) = setDescription(context.getString(resId))
+
 
 fun FloatingActionButton.setDescription(text: String) {
     contentDescription = text
