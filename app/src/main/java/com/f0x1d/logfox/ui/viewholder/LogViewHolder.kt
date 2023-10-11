484839bde52379653fda855af7a9b7a40f17c98b
Jesse205 <2055675594@qq.com>
2023-10-11 22:29:26 +0800

    refactor: use drawable xml in log item

--- app/src/main/java/com/f0x1d/logfox/ui/viewholder/LogViewHolder.kt
+++ app/src/main/java/com/f0x1d/logfox/ui/viewholder/LogViewHolder.kt
@@ -1,11 +1,9 @@
 package com.f0x1d.logfox.ui.viewholder
 
 import android.content.res.ColorStateList
-import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.GradientDrawable
 import android.view.Gravity
 import androidx.appcompat.widget.PopupMenu
-import androidx.core.graphics.ColorUtils
 import com.f0x1d.logfox.R
 import com.f0x1d.logfox.adapter.LogsAdapter
 import com.f0x1d.logfox.databinding.ItemLogBinding
@@ -14,16 +12,11 @@ import com.f0x1d.logfox.extensions.logline.foregroundColorByLevel
 import com.f0x1d.logfox.model.LogLine
 import com.f0x1d.logfox.ui.viewholder.base.BaseViewHolder
 import com.f0x1d.logfox.utils.dpToPx
-import com.google.android.material.color.MaterialColors
 
 class LogViewHolder(
     binding: ItemLogBinding,
     private val copyLog: (LogLine) -> Unit
 ): BaseViewHolder<LogLine, ItemLogBinding>(binding) {
-
-    private val currentColorPrimary = MaterialColors.getColor(binding.root, com.google.android.material.R.attr.colorPrimary)
-    private val background = binding.container.background
-    private val selectedBackground = ColorDrawable(ColorUtils.blendARGB((background as ColorDrawable).color, currentColorPrimary, 0.2f))
 
     private val radius = 6.dpToPx
 
@@ -116,6 +109,6 @@ class LogViewHolder(
 
     private fun changeExpandedAndSelected(logLine: LogLine) {
         binding.logText.maxLines = if (adapter<LogsAdapter>().expandedStates.getOrElse(logLine.id) { adapter<LogsAdapter>().logsExpanded }) Int.MAX_VALUE else 1
-        binding.container.background = if (adapter<LogsAdapter>().selectedItems.contains(logLine)) selectedBackground else background
+        binding.container.isSelected = adapter<LogsAdapter>().selectedItems.contains(logLine)
     }
 }
