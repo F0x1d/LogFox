24dc15e4ef737388e3510785cb04363c64108310
Jesse205 <2055675594@qq.com>
2023-10-15 19:30:48 +0800

    refactor: split ViewExtensions.kt

--- app/src/main/java/com/f0x1d/logfox/extensions/views/ViewAccessibilityExtensions.kt
+++ app/src/main/java/com/f0x1d/logfox/extensions/views/ViewAccessibilityExtensions.kt
@@ -2,9 +2,6 @@ package com.f0x1d.logfox.extensions.views
 
 import android.view.View
 import android.view.accessibility.AccessibilityNodeInfo
-import com.google.android.material.snackbar.Snackbar
-
-fun View.snackbar(text: String) = Snackbar.make(this, text, Snackbar.LENGTH_SHORT).show()
 
 fun View.setAccessibilityDelegateForClassName(clazz: Class<out View>) {
     accessibilityDelegate = object: View.AccessibilityDelegate() {
