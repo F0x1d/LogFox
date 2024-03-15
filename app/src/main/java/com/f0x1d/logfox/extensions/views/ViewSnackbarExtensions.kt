f039cb4e8b6c756461c6774c77288895474be631
Jesse205 <51242302+Jesse205@users.noreply.github.com>
2024-03-15 14:54:33 +0800

    [fix]: snackbar margin (#112)

--- app/src/main/java/com/f0x1d/logfox/extensions/views/ViewSnackbarExtensions.kt
+++ app/src/main/java/com/f0x1d/logfox/extensions/views/ViewSnackbarExtensions.kt
@@ -3,4 +3,4 @@ package com.f0x1d.logfox.extensions.views
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
 
-fun View.snackbar(text: String) = Snackbar.make(this, text, Snackbar.LENGTH_SHORT).show()
+fun View.snackbar(text: String) = Snackbar.make(this, text, Snackbar.LENGTH_SHORT).apply { show() }
