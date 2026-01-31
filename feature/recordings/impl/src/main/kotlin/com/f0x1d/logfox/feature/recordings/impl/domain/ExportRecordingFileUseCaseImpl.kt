7992f5d27dd8107ed6074a66bb6d16e7ba174576
F0x1d <i@f0x1d.com>
2026-02-01 02:04:11 +0300

    cool

--- feature/recordings/impl/src/main/kotlin/com/f0x1d/logfox/feature/recordings/impl/domain/ExportRecordingFileUseCaseImpl.kt
+++ feature/recordings/impl/src/main/kotlin/com/f0x1d/logfox/feature/recordings/impl/domain/ExportRecordingFileUseCaseImpl.kt
@@ -1,7 +1,9 @@
 package com.f0x1d.logfox.feature.recordings.impl.domain
 
 import android.net.Uri
+import com.f0x1d.logfox.core.context.deviceData
 import com.f0x1d.logfox.feature.export.api.data.ExportRepository
+import com.f0x1d.logfox.feature.preferences.api.domain.service.GetIncludeDeviceInfoInArchivesUseCase
 import com.f0x1d.logfox.feature.recordings.api.data.RecordingsRepository
 import com.f0x1d.logfox.feature.recordings.api.domain.ExportRecordingFileUseCase
 import javax.inject.Inject
@@ -9,10 +11,17 @@ import javax.inject.Inject
 internal class ExportRecordingFileUseCaseImpl @Inject constructor(
     private val recordingsRepository: RecordingsRepository,
     private val exportRepository: ExportRepository,
+    private val getIncludeDeviceInfoInArchivesUseCase: GetIncludeDeviceInfoInArchivesUseCase,
 ) : ExportRecordingFileUseCase {
 
     override suspend fun invoke(recordingId: Long, uri: Uri) {
         val recording = recordingsRepository.getById(recordingId) ?: return
-        exportRepository.copyFileToUri(uri, recording.file)
+
+        if (getIncludeDeviceInfoInArchivesUseCase()) {
+            val prefix = deviceData + "\n\n"
+            exportRepository.writeContentAndFileToUri(uri, prefix, recording.file)
+        } else {
+            exportRepository.copyFileToUri(uri, recording.file)
+        }
     }
 }
